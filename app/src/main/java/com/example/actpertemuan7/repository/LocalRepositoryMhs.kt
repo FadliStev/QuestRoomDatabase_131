5799e9b0d13ccb356a3a06008c8485d2dd6caea4
Wiranto Akbar Fadlillah <alvinex1999@gmail.com>
2024-12-16 13:11:39 +0700

    menambahkan metode getall, get by nim, delete, dan update

--- app/src/main/java/com/example/actpertemuan7/repository/LocalRepositoryMhs.kt
+++ app/src/main/java/com/example/actpertemuan7/repository/LocalRepositoryMhs.kt
@@ -2,6 +2,7 @@ package com.example.actpertemuan7.repository
 
 import com.example.actpertemuan7.data.dao.MahasiswaDao
 import com.example.actpertemuan7.data.entity.Mahasiswa
+import kotlinx.coroutines.flow.Flow
 
 class LocalRepositoryMhs (
     private val mahasiswaDao: MahasiswaDao
@@ -9,4 +10,21 @@ class LocalRepositoryMhs (
     override suspend fun insertMhs(mahasiswa: Mahasiswa) {
        mahasiswaDao.insertMahasiswa(mahasiswa)
     }
+    override fun getAllMhs(): Flow<List<Mahasiswa>>{
+        return mahasiswaDao.getAllMahasiswa()
+    }
+
+    override fun getMhs(nim: String): Flow<Mahasiswa>{
+        return mahasiswaDao.getMahasiswa(nim)
+    }
+
+    override suspend fun deleteMhs(mahasiswa: Mahasiswa) {
+        return mahasiswaDao.deleteMahasiswa(mahasiswa)
+    }
+
+    override suspend fun updateMhs(mahasiswa: Mahasiswa) {
+        return mahasiswaDao.updateMahasiswa(mahasiswa)
+    }
+
+
 }
