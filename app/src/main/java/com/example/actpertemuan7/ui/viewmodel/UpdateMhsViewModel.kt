3d42a6504f0e6f1dfd0146f21b132ff0ed22cd3c
Wiranto Akbar Fadlillah <alvinex1999@gmail.com>
2024-12-17 09:33:05 +0700

    menambahkan function updatemhs

--- app/src/main/java/com/example/actpertemuan7/ui/viewmodel/UpdateMhsViewModel.kt
+++ app/src/main/java/com/example/actpertemuan7/ui/viewmodel/UpdateMhsViewModel.kt
@@ -1,17 +1,38 @@
 package com.example.actpertemuan7.ui.viewmodel
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.actpertemuan7.data.entity.Mahasiswa
 import com.example.actpertemuan7.repository.RepositoryMhs
 import com.example.actpertemuan7.ui.navigation.DestinasiUpdate
+import com.example.actpertemuan7.ui.view.mahasiswa.InsertBodyMhs
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import com.example.actpertemuan7.ui.customwidget.TopAppBar as TopAppBar
 
 
 class UpdateMhsViewModel(
@@ -77,12 +98,83 @@ class UpdateMhsViewModel(
                 snackbarMessage = "Data gagal diupdate"
             )
         }
-        fun resetSnackBarMessage(){
-            updateUIState = updateUIState.copy(snackbarMessage = null)
+
+    }
+    fun resetSnackBarMessage(){
+        updateUIState = updateUIState.copy(snackbarMessage = null)
+    }
+
+}
+fun Mahasiswa.toUIStateMhs(): MhsUIState = MhsUIState(
+    mahasiswaEvent = this.toDetailUiEvent(),
+)
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun UpdateMhsView(
+    onBack: () -> Unit,
+    onNavigate: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: UpdateMhsViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+    val uiState = viewModel.updateUIState
+    val snackbarHostState = remember {
+        SnackbarHostState()
+    }
+    val coroutineScope = rememberCoroutineScope()
+
+    LaunchedEffect(uiState.snackbarMessage) {
+        println("Launched Effect Triggered")
+        uiState.snackbarMessage?.let { message ->
+            println("Snackbar Message Received: $message")
+            coroutineScope.launch {
+                println("Launching coroutinr for snackbar")
+                snackbarHostState.showSnackbar(
+                    message = message,
+                    duration = SnackbarDuration.Long
+                )
+                viewModel.resetSnackBarMessage()
+            }
         }
     }
-    fun Mahasiswa.toUIStateMhs(): MhsUIState = MhsUIState(
-        mahasiswaEvent = this.toDetailUiEvent(),
-    )
+
+    Scaffold(
+        modifier = modifier,
+        snackbarHost = { SnackbarHost(hostState = snackbarHostState) },
+        topBar = {
+            TopAppBar(
+                onBack = onBack,
+                judul = "Edit Mahasiswa",
+                showBackButton = true,
+                modifier = modifier
+            )
+        }
+    ) {padding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding)
+                .padding(16.dp)
+        ) {
+            InsertBodyMhs(
+                uiState = uiState,
+                onValueChange = { updateEvent ->
+                    viewModel.updateState(updateEvent)
+                },
+                onClick = {
+                    coroutineScope.launch {
+                        if(viewModel.validateFields()){
+                        viewModel.updateData()
+                        delay(600)
+                        withContext(Dispatchers.Main){
+                            onNavigate()
+                        }
+                        }
+                    }
+                }
+            )
+        }
+
+    }
 }
 
